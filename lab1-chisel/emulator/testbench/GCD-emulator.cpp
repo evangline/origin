756f12512ff6cbcb8892d0949f218b1ce4eb4681
Rimas Avizienis <rimas@eecs.berkeley.edu>
2012-08-30 15:25:20 -0700

    cleaned up GCD-emulator.cpp Chisel C++ testbench

--- lab1-chisel/emulator/testbench/GCD-emulator.cpp
+++ lab1-chisel/emulator/testbench/GCD-emulator.cpp
@@ -1,41 +1,43 @@
-//#include "../generated-src/GCD.h"
 #include "GCD.h"
+
+// uncomment next line to enable trace output
+#define DEBUG
+
+// Define test vectors
+// Could also read these from a file, or generate using a script
+static uint32_t src[][2]  = { {15, 27} , { 49, 21 }, { 30, 25 }, { 27, 19 }, { 40, 40 }, { 190, 250 }, { 250, 5 }, { 0, 0 } };
+static uint32_t sink[] = { 3, 7, 5, 1, 40, 10, 5, 0 };
 
 int main (int argc, char* argv[]) {
   // Load our design
   GCD_t* c = new GCD_t();
-  // Limit of cycles (increase if not finishing test)
+
+  // Max number of clock cycles to simulate before exiting
   int lim = 1000;
+
+  // initialization
   c->init();
 
-  // Define test vectors
-  // Can also read these from a file, or use a script to generate
-  // this text
-  uint32_t num_tests = 8;
-  uint32_t src[num_tests][2]; uint32_t sink[num_tests];
-  src[0][1] = 27; src[0][0] = 15; sink[0] = 3;
-  src[1][1] = 21; src[1][0] = 49; sink[1] = 7;
-  src[2][1] = 25; src[2][0] = 30; sink[2] = 5;
-  src[3][1] = 19; src[3][0] = 27; sink[3] = 1;
-  src[4][1] = 40; src[4][0] = 40; sink[4] = 40;
-  src[5][1] = 250; src[5][0] = 190; sink[5] = 10;
-  src[6][1] = 5; src[6][0] = 250; sink[6] = 5;
-  src[7][1] = 0; src[7][0] = 0; sink[7] = 0;
+  uint32_t num_tests = sizeof(sink) / sizeof(uint32_t);
   uint32_t testCount = 0;
 
-  // Every loop is a cycle
-  for (int t = 0; lim < 0 || t < lim; t++) {
+  int cycle;
+  int done = 0;
+  // Every loop is one clock cycle
+  for (cycle = 0; lim < 0 || cycle < lim && !done ; cycle++) {
 
-    dat_t<1> reset = LIT<1>(t==0);
+    dat_t<1> reset = LIT<1>(cycle==0);
 
-    // input is always valid
-    c->GCD__io_operands_val = LIT<1>(1);
 
-    // Run tests
-    // Set test vector at input
+    // Set input ports to test vector values
     c->GCD__io_operands_bits_A = LIT<16>(src[testCount][1]);
     c->GCD__io_operands_bits_B = LIT<16>(src[testCount][0]);
-    c->GCD__io_operands_val = LIT<1>(1);
+
+    // assert operands_val as long as there are still tests to run
+    c->GCD__io_operands_val = LIT<1>(testCount < num_tests);
+
+    // always ready for output
+//    c->GCD__io_operands_rdy = LIT<1>(1);
 
     // Evaluate the data
     // Note: this is the same as a positive edge clock tick occuring
@@ -44,54 +46,82 @@ int main (int argc, char* argv[]) {
     // flow through the logic in the same cycle.
     // Therefore, we will advance the input operands immediately after
     // the DUT says that they are received (instead of advancing the operands
-    // when results_val goes high, becaues by then it is too late)
-    // This only matters if results_val, and operands_rdy can go high on the
+    // when result_val goes high, becaues by then it is too late)
+    // This only matters if result_val, and operands_rdy can go high on the
     // same cycle
+
     c->clock_lo(reset);
 
     // Check result and print
-    if((c->GCD__io_results_val.lo_word() == 1) && (testCount > 0)){
-      uint32_t results_bits = c->GCD__io_results_bits_data.lo_word();
-      if (results_bits == sink[testCount-1]) {
-        printf("[Passed] Test #%d: GCD(%d,%d)=%d\n",testCount,src[testCount-1][1],src[testCount-1][0],results_bits);
-      } else {
+    if((c->GCD__io_result_val.lo_word() == 1) && (testCount > 0))
+    {
+      uint32_t result_bits = c->GCD__io_result_bits_data.lo_word();
+
+      if (result_bits == sink[testCount-1]) 
+      {
+        printf("[Passed] Test #%d: GCD(%d,%d)=%d\n",testCount,src[testCount-1][1],src[testCount-1][0],result_bits);
+      } 
+      else 
+      {
         printf("[Failed]\n");
-        printf("Reason: %d != %d\n",results_bits,sink[testCount-1]);
+        printf("Reason: %d != %d\n",result_bits,sink[testCount-1]);
       }
+
       // Quit if done with all tests
-      if(testCount == num_tests){ return 0; }
+      if(testCount == num_tests)
+        done=1;
+
     }
 
     // Start next test once DUT gets data
-    if((c->GCD__io_operands_rdy.lo_word() == 1) && (reset.lo_word() == 0)){
-    testCount = testCount + 1;
-    }
+    if((c->GCD__io_operands_rdy.lo_word() == 1) && (reset.lo_word() == 0))
+      testCount++;
 
-    // Print debugging code (uncomment to enable)
-    // If possible additional signal names are commended out.
-    // Format will be c->GCD_signalname.lo_word() where signalname
-    // is the name given in Chisel
-  /*
-    uint32_t operands_val = c->GCD__io_operands_val.lo_word();
-    uint32_t operands_rdy = c->GCD__io_operands_rdy.lo_word();
-    uint32_t results_val = c->GCD__io_results_val.lo_word();
-    uint32_t results_bits = c->GCD__io_results_bits_data.lo_word();
-    uint32_t A_in = c->GCD__io_operands_bits_A.lo_word();
-    uint32_t B_in = c->GCD__io_operands_bits_B.lo_word();
-
-    uint32_t A_reg = c->GCD_A_reg.lo_word();
-    uint32_t B_reg = c->GCD_B_reg.lo_word();
-    uint32_t A_reg_next = c->GCD_A_reg_shadow.lo_word();
-    uint32_t B_reg_next = c->GCD_B_reg_shadow.lo_word();
-    //uint32_t state = c->GCD_state.lo_word();
-    //uint32_t first = c->GCD_first.lo_word();
-    
-    //printf("State: %d, Inval: %d, Inrdy: %d, A_in: %d, B_in: %d,  A: %d; B: %d; result_val: %d; result:%d\n",state,operands_val,operands_rdy,A_in,B_in,A_reg,B_reg,results_val, results_bits);
-    printf("Reset: %d, operands_val: %d, operands_rdy: %d, A_in: %d, B_in: %d,  A: %d (next: %d); B: %d (next: %d); result_val: %d; result:%d\n",reset.lo_word(),operands_val,operands_rdy,A_in,B_in,A_reg,A_reg_next,B_reg,B_reg_next,results_val, results_bits);
-
-    */
-    // Next stage
     c->clock_hi(reset);
 
-  }
+    // Print trace for debugging
+    // Variables that hold signal values named like:
+    // c->GCD__signalname.lo_word() 
+    // where signalname is a (mangled) name of a signal inside your Chisel code
+    // look in generated-src/GCD.h to see all variables associated with signals
+    
+#ifdef DEBUG
+//  Print the values of the input and output signals
+    uint32_t operands_val = c->GCD__io_operands_val.lo_word();
+    uint32_t operands_rdy = c->GCD__io_operands_rdy.lo_word();
+    uint32_t result_val   = c->GCD__io_result_val.lo_word();
+//    uint32_t result_rdy   = c->GCD__io_result_rdy.lo_word();
+    uint32_t result_bits  = c->GCD__io_result_bits_data.lo_word();
+    uint32_t A_in         = c->GCD__io_operands_bits_A.lo_word();
+    uint32_t B_in         = c->GCD__io_operands_bits_B.lo_word();
+
+    printf("cycle: %d\t operands_val: %d\t operands_rdy: %d\t A_in: %d\t B_in: %d\t result_val: %d\t result_bits: %d\n", \
+        cycle, operands_val, operands_rdy, A_in, B_in, result_val, result_bits);
+
+//    printf("reset: %d\t operands_val: %d\t operands_rdy: %d\t A_in: %d\t B_in: %d\t result_val: %d\t, result_rdy: %d\t results_bits: %d\t\n", \
+        reset.lo_word(), operands_val, operands_rdy, A_in, B_in, result_val, result_rdy, result_bits);
+
+//    Add internal signals here, if you want to see them
+//    these are just examples, your code probably won't have signals named
+//    like these
+
+//    uint32_t A_reg        = c->GCD_A_reg.lo_word();
+//    uint32_t B_reg        = c->GCD_B_reg.lo_word();
+//    uint32_t A_reg_next   = c->GCD_A_reg_shadow.lo_word();
+//    uint32_t B_reg_next   = c->GCD_B_reg_shadow.lo_word();
+//    uint32_t state        = c->GCD_state.lo_word();
+//    uint32_t first        = c->GCD_first.lo_word();
+
+//    add a printf here to see the signals
+#endif
+ }
+
+ if(done)
+ {
+    printf("All tests passed, simulation finished after %d cycles.\n", cycle);
+    return 0;
+ }
+
+  printf("[Failed] Timed out after %d cycles!\n", cycle);
+  return -1;
 }
