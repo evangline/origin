2cda9888d25dc052698c1db72db03d4b9aa4b7ed
Rimas Avizienis <rimas@eecs.berkeley.edu>
2012-08-31 02:59:04 -0700

    updated build.scala to fetch Chisel jar file instead of rebuilding it

--- lab1-chisel/sbt/project/build.scala
+++ lab1-chisel/sbt/project/build.scala
@@ -1,19 +1,21 @@
 import sbt._
 import Keys._
-
+  
 object BuildSettings
 {
   val buildOrganization = "edu.berkeley.cs"
   val buildVersion = "1.1"
   val buildScalaVersion = "2.9.2"
-  // val buildScalaVersion = "2.10.0-M4"
 
   def apply(projectdir: String) = {
     Defaults.defaultSettings ++ Seq (
       organization := buildOrganization,
       version      := buildVersion,
       scalaVersion := buildScalaVersion,
-      scalaSource in Compile := Path.absolute(file(projectdir + "/src/main/scala"))
+      scalaSource in Compile := Path.absolute(file(projectdir + "/src/main/scala")),
+      resourceDirectory in Compile := Path.absolute(file(projectdir + "/src/main/resources/")),
+      resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      libraryDependencies += "edu.berkeley.cs" %% "chisel" % "1.0"
     )
   }
 }
@@ -21,8 +23,5 @@ object BuildSettings
 object ChiselBuild extends Build
 {
   import BuildSettings._
-
-  lazy val chisel = Project("chisel", file("chisel"), settings = BuildSettings(java.lang.System.getenv("CHISEL")))
-//  lazy val tutorial = Project("work", file("work"), settings = BuildSettings("..")) dependsOn(chisel)
-  lazy val work = Project("work", file("work"), settings = BuildSettings(".")) dependsOn(chisel)
+  lazy val work = Project("work", file("work"), settings = BuildSettings("."))
 }
