e96e4e949f847bafcfeff3f770cad7e54b1fa06d
Rimas Avizienis <rimas@eecs.berkeley.edu>
2012-09-15 01:02:07 -0700

    minor tweaks to test harness to make it work for gate level simulation

--- lab2/vlsi/testbench/medianFilterTestHarness_rtl.v
+++ lab2/vlsi/testbench/medianFilterTestHarness_rtl.v
@@ -19,8 +19,10 @@ module medianFilterTestHarness_rtl;
     reset <= reset_ext;
 
   // instantiate and connect median filter module
-  reg frame_sync_in = 1'b0;
-  reg [7:0] data_in = 8'd0;
+  reg frame_sync_in_reg = 1'b0;
+  wire #0.1 frame_sync_in = frame_sync_in_reg;
+  reg [7:0] data_in_reg = 8'd0;
+  wire [7:0] #0.1 data_in = data_in_reg;
   wire [7:0] data_out;
   wire frame_sync_out;
 
@@ -39,11 +41,10 @@ module medianFilterTestHarness_rtl;
   // get command line options, initialize simulation
   reg [31:0] max_cycles;
   reg [31:0] num_images;
+  reg [31:0] vcdpluson;
 
   initial
   begin
-    // turn on VPD trace file generation
-    $vcdpluson(0);
 
     // Get number of test images to try before finishing
     if (!$value$plusargs("num-images=%d", num_images))
@@ -54,6 +55,11 @@ module medianFilterTestHarness_rtl;
     // images
     if (!$value$plusargs("max-cycles=%d", max_cycles))
       max_cycles = (num_images * `IMAGE_SIZE) + (`IMAGE_WIDTH*3);
+
+    // turn on VPD trace file generation
+    if ($value$plusargs("vcdpluson=%d", vcdpluson))
+      if (vcdpluson != 0) 
+        $vcdpluson(0);
 
     // Strobe reset
     reset_ext = 1;
@@ -93,75 +99,74 @@ module medianFilterTestHarness_rtl;
   reg mismatch = 1'b0;
   reg [31:0] input_offset = 32'd0;
   reg [31:0] output_offset = 32'd0;
-  reg [31:0] data_in_reg;
+  reg [31:0] data_in32;
   reg [31:0] correct_dout = 32'd0;
   reg [31:0] image_count = 32'd0;
 
   always @(posedge clk)
   begin
-    if (cycle_count > 0)
+    if (cycle_count > 1)
     begin
       if (input_offset == 0)
       begin
         // generate a new random test input image
         generate_input_image();
-        frame_sync_in <= 1'b1;
+        frame_sync_in_reg <= 1'b1;
       end
       else
-        frame_sync_in <= 1'b0;
+        frame_sync_in_reg <= 1'b0;
 
       // get value of input image at specified offset
-      get_input_pixel(input_offset, data_in_reg);
-      data_in <= data_in_reg[7:0];
+      get_input_pixel(input_offset, data_in32);
+      data_in_reg <= data_in32[7:0];
 
       if (input_offset == `IMAGE_SIZE-1)
         input_offset <= 0;
       else
         input_offset <= input_offset + 1;
 
-    end
-
-    if (frame_sync_out && !started)
-    begin
-      // filter input image to produce expected output image
-      generate_output_image();
-      started <= 1'b1;
-    end
-
-    if (started || frame_sync_out)
-    begin
-      // get value of expected output image at specified offset
-      get_output_pixel(output_offset, correct_dout);
-      if (data_out != correct_dout[7:0])
+      if (frame_sync_out && !started)
       begin
-        $display("ERROR: Mismatch at cycle %d : expected %02x : actual %02x", cycle_count, correct_dout, data_out);
-        failed <= 1'b1;
-        image_failed <= 1'b1;
-        mismatch <= 1'b1;
-      end
-      else
-        mismatch <= 1'b0;
-
-      if (output_offset == `IMAGE_SIZE-1)
-      begin
-        if (image_failed)
-          $display("[FAILED] Filter output for input image %d was incorrect!", image_count+1);
-        else
-          $display("[PASSED] Image %d processed succesfully.", image_count+1);
-
-        image_failed <= 1'b0;
-
-        if (image_count == num_images-1)
-            done <= 1'b1;
-
-        image_count <= image_count + 1;
-        output_offset <= 0;
-
-        // produced expected output image for next input image
+        // filter input image to produce expected output image
         generate_output_image();
+        started <= 1'b1;
       end
-      else
-        output_offset <= output_offset + 1;
+
+      if (started || frame_sync_out)
+      begin
+        // get value of expected output image at specified offset
+        get_output_pixel(output_offset, correct_dout);
+        if (data_out != correct_dout[7:0])
+        begin
+          $display("ERROR: Mismatch at cycle %d : expected %02x : actual %02x", cycle_count, correct_dout, data_out);
+          failed <= 1'b1;
+          image_failed <= 1'b1;
+          mismatch <= 1'b1;
+        end
+        else
+          mismatch <= 1'b0;
+
+        if (output_offset == `IMAGE_SIZE-1)
+        begin
+          if (image_failed)
+            $display("[FAILED] Filter output for input image %d was incorrect!", image_count+1);
+          else
+            $display("[PASSED] Image %d processed succesfully.", image_count+1);
+
+          image_failed <= 1'b0;
+
+          if (image_count == num_images-1)
+              done <= 1'b1;
+
+          image_count <= image_count + 1;
+          output_offset <= 0;
+
+          // produced expected output image for next input image
+          generate_output_image();
+        end
+        else
+          output_offset <= output_offset + 1;
+      end
     end
   end
 
