2cc17ca5168fa1f1d9ecdb43cf9c6e7242201688
pfchiu <pi.feng.chiu@gmail.com>
2012-09-15 21:29:52 -0700

    COMMIT median.scala

--- lab2/sbt/src/main/scala/median.scala
+++ lab2/sbt/src/main/scala/median.scala
@@ -21,9 +21,74 @@ class median9(w: Integer) extends Component {
     val p9 = UFix(INPUT, w)
     val median = UFix(OUTPUT, w)
   }
-  // YOUR CODE GOES HERE
-  // erase the next line
-  io.median := io.p5;
+  val srt0 = new ele3sort(w)
+  val srt1 = new ele3sort(w)
+  val srt2 = new ele3sort(w)
+  val srt3 = new ele3sort(w)
+  val srt4 = new ele3sort(w)
+  val srt5 = new ele3sort(w)
+  val srt6 = new ele3sort(w)
+
+  srt0.io.in0 := io.p1
+  srt0.io.in1 := io.p2
+  srt0.io.in2 := io.p3
+  srt1.io.in0 := io.p4
+  srt1.io.in1 := io.p5
+  srt1.io.in2 := io.p6
+  srt2.io.in0 := io.p7
+  srt2.io.in1 := io.p8
+  srt2.io.in2 := io.p9
+  srt3.io.in0 <> srt0.io.max
+  srt3.io.in1 <> srt1.io.max
+  srt3.io.in2 <> srt2.io.max
+  srt4.io.in0 <> srt0.io.med
+  srt4.io.in1 <> srt1.io.med
+  srt4.io.in2 <> srt2.io.med
+  srt5.io.in0 <> srt0.io.min
+  srt5.io.in1 <> srt1.io.min
+  srt5.io.in2 <> srt2.io.min
+  srt6.io.in0 <> srt3.io.min
+  srt6.io.in1 <> srt4.io.med
+  srt6.io.in2 <> srt5.io.max
+  io.median := srt6.io.med
+}
+
+
+class ele3sort(w:Integer) extends Component{
+  val io = new Bundle{
+    val in0 = UFix(INPUT,w)
+    val in1 = UFix(INPUT,w)
+    val in2 = UFix(INPUT,w)
+    val max = UFix(OUTPUT,w)
+    val med = UFix(OUTPUT,w)
+    val min = UFix(OUTPUT,w)
+  }
+  val comp0 = new ele2sort(w)
+  val comp1 = new ele2sort(w)
+  val comp2 = new ele2sort(w)
+
+  comp0.io.in0 := io.in0
+  comp0.io.in1 := io.in1
+  comp1.io.in0 := io.in2
+  comp1.io.in1 <> comp0.io.max
+  comp2.io.in0 <> comp0.io.min
+  comp2.io.in1 <> comp1.io.min
+
+  io.max := comp1.io.max
+  io.med := comp2.io.max
+  io.min := comp2.io.min
+}
+
+class ele2sort(w:Integer) extends Component{
+  val io = new Bundle{
+    val in0 = UFix(INPUT,w)
+    val in1 = UFix(INPUT,w)
+    val max = UFix(OUTPUT,w)
+    val min = UFix(OUTPUT,w)
+  } 
+  val gt = io.in0 > io.in1
+  io.max := Mux(gt, io.in0, io.in1)
+  io.min := Mux(gt, io.in1, io.in0)
 }
 
 class median9Test(c: median9) extends Tester(c, Array(c.io)) {
